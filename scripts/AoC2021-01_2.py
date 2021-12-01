5acacacd470f735f8d5d92e2b9788cc7a6c82308
Patato777 <jean.sma@free.fr>
2021-12-01 10:46:57 +0100

    Improved it

--- scripts/AoC2021-01_2.py
+++ scripts/AoC2021-01_2.py
@@ -2,15 +2,10 @@ previous_sum = float('inf')
 increased = 0
 
 with open('../inputs/AoC2021-01.txt', 'r') as f:
-    d1, d2 = int(f.readline()), int(f.readline())
-    previous = [d1 + d2, d2]
+    queue = [int(f.readline()) for _ in range(3)]
     for line in f:
-        previous.append(0)
-        for i in range(3):
-            previous[i] += int(line)
-        new_sum = previous.pop(0)
-        if new_sum > previous_sum:
+        queue.append(int(line))
+        if queue.pop(0) < queue[-1]:
             increased += 1
-        previous_sum = new_sum
 
 print(increased)
